806ecc98097afc26c2755b3f7e557a46f95d582f
Amitesh Singh <amiteshsinghk@gmail.com>
2024-11-03 19:31:14 +0530

    IllegalStateException Fixed:The application crashes when the user moves it to the background and brings it back to the foreground.

--- mydatabase/src/main/java/com/example/mydatabase/domain/note/Note.kt
+++ mydatabase/src/main/java/com/example/mydatabase/domain/note/Note.kt
@@ -1,5 +1,7 @@
 package com.example.mydatabase.domain.note
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.example.mydatabase.presentation.*
 import kotlinx.datetime.LocalDateTime
 
@@ -9,10 +11,81 @@ data class Note(
     val content: String,
     val colorHex: Long,
     val created: LocalDateTime
-) {
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readLong(),
+        LocalDateTime.parse(parcel.readString()!!)
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        if (id != null) {
+            parcel.writeLong(id)
+        }
+        parcel.writeString(title)
+        parcel.writeString(content)
+        parcel.writeLong(colorHex)
+        parcel.writeString(created.toString())
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Note> {
+        private val colors = listOf(RedOrangeHex, RedPinkHex, LightGreenHex, BabyBlueHex, VioletHex)
+
+        fun generateRandomColor() = colors.random()
+        override fun createFromParcel(parcel: Parcel): Note {
+            return Note(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Note?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+/*data class Note(
+    val id: Long?,
+    val title: String,
+    val content: String,
+    val colorHex: Long,
+    val created: LocalDateTime
+): Parcelable {
     companion object {
         private val colors = listOf(RedOrangeHex, RedPinkHex, LightGreenHex, BabyBlueHex, VioletHex)
 
         fun generateRandomColor() = colors.random()
     }
-}
+
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(Long::class.java.classLoader) as? Long,
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readLong(),
+        TODO("created")
+    ) {
+    }
+
+    override fun describeContents(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun writeToParcel(p0: Parcel, p1: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object CREATOR : Parcelable.Creator<Note> {
+        override fun createFromParcel(parcel: Parcel): Note {
+            return Note(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Note?> {
+            return arrayOfNulls(size)
+        }
+    }
+}*/
